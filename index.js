7daec48c19817a229603ea7a2b56b7bbcfa9ae36
ken <kenniyu@gmail.com>
2016-07-27 15:30:26 -0700

    Packagejson

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(8000, function() {
+http.listen(process.env.PORT || 5000, function() {
   console.log('listening on *:8000');
 });
 
